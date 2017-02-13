0e56ba3265446a906ecec402cedb3bae6d1bc47e
Joel Messerli <joel@jmnw.me>
2017-02-13 22:01:27 +0100

    Fixed array not having quotes

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
-<?php
-use Carbon\Carbon;
+<?php require_once 'vendor/autoload.php';
 
-require_once 'vendor/autoload.php';
+use Carbon\Carbon;
 
 Flight::route('/', function () {
     $now = Carbon::now('Europe/Zurich');
 
     Flight::json([
-        date_time => $now->toIso8601String(),
-        week_of_year => $now->weekOfYear
+        'date_time' => $now->toIso8601String(),
+        'week_of_year' => $now->weekOfYear
     ]);
 });
 
